03ab78490714c161de7792874afc1014ef17f8e8
yeshwanthvr <yeshwanthvr@hotmail.com>
2021-03-06 19:58:13 +0530

    Re-programmed a big code into a simpler code.

--- Divisible.java
+++ Divisible.java
@@ -11,149 +11,19 @@ public class Divisible {
         {
             Q[i] = sc.nextLong();
         }
-        System.out.println("Pick an option from below :-");
-        for(long i = 1000000000; i >= 0; i--);
-        for(long i = 1000000000; i >= 0; i--);
-        System.out.println("\nEnter 1 to check for the divisibility test by 2");
-        System.out.println("Enter 2 to check for the divisibility test by 3");
-        System.out.println("Enter 3 to check for the divisibility test by 4");
-        System.out.println("Enter 4 to check for the divisibility test by 5");
-        System.out.println("Enter 5 to check for the divisibility test by 6");
-        System.out.println("Enter 6 to check for the divisibility test by 8");
-        System.out.println("Enter 7 to check for the divisibility test by 9");
-        System.out.println("Enter 8 to check for the divisibility test by 10");
-        System.out.println("Enter 9 to check for the divisibility test by 11");
+        System.out.println("By which number do you want to check the divisibility of the numbers : ");
         int choice = sc.nextInt();
-        switch(choice)
+        System.out.println("Numbers divisible by " +choice+ "\t\t\tNumbers not divisible by " +choice);
+        for(int i = 0; i < n; i++)
         {
-            case 1:
-            for(int i = 0; i < Q.length; i++)
+            if(Q[i] % choice == 0)
             {
-                if(Q[i] % 2 == 0)
-                {
-                    System.out.println(+Q[i]+ " is divisible by 2.");
-                }
-                else
-                {
-                    System.out.println(+Q[i]+ " is not divisible by 2");
-                }
+                System.out.println(+Q[i]+"\t\t\t\t\t\t----------");
             }
-            break;   // end of case 1
-
-            case 2:
-            for(int i = 0; i < Q.length; i++)
+            else
             {
-                if(Q[i] % 3 == 0)
-                {
-                    System.out.println(+Q[i]+ " is divisible by 3.");
-                }
-                else
-                {
-                    System.out.println(+Q[i]+ " is not divisible by 3");
-                }
+                System.out.println("----------\t\t\t\t\t\t" +Q[i]);
             }
-            break;   // end of case 2
-
-            case 3:
-            for(int i = 0; i < Q.length; i++)
-            {
-                if(Q[i] % 4 == 0)
-                {
-                    System.out.println(+Q[i]+ " is divisible by 4.");
-                }
-                else
-                {
-                    System.out.println(+Q[i]+ " is not divisible by 4");
-                }
-            }
-            break;  // end of case 3
-
-            case 4:
-            for(int i = 0; i < Q.length; i++)
-            {
-                if(Q[i] % 5 == 0)
-                {
-                    System.out.println(+Q[i]+ " is divisible by 5.");
-                }
-                else
-                {
-                    System.out.println(+Q[i]+ " is not divisible by 5");
-                }
-            }
-            break;  // end of case 4
-
-            case 5:
-            for(int i = 0; i < Q.length; i++)
-            {
-                if(Q[i] % 6 == 0)
-                {
-                    System.out.println(+Q[i]+ " is divisible by 6.");
-                }
-                else
-                {
-                    System.out.println(+Q[i]+ " is not divisible by 6");
-                }
-            }
-            break;  // end of case 5
-
-            case 6:
-            for(int i = 0; i < Q.length; i++)
-            {
-                if(Q[i] % 8 == 0)
-                {
-                    System.out.println(+Q[i]+ " is divisible by 8.");
-                }
-                else
-                {
-                    System.out.println(+Q[i]+ " is not divisible by 8");
-                }
-            }
-            break;  // end of case 6
-
-            case 7:
-            for(int i = 0; i < Q.length; i++)
-            {
-                if(Q[i] % 9 == 0)
-                {
-                    System.out.println(+Q[i]+ " is divisible by 9.");
-                }
-                else
-                {
-                    System.out.println(+Q[i]+ " is not divisible by 9");
-                }
-            }
-            break;
-
-            case 8:
-            for(int i = 0; i < Q.length; i++)
-            {
-                if(Q[i] % 10 == 0)
-                {
-                    System.out.println(+Q[i]+ " is divisible by 10.");
-                }
-                else
-                {
-                    System.out.println(+Q[i]+ " is not divisible by 10");
-                }
-            }
-            break;
-
-            case 9:
-            for(int i = 0; i < Q.length; i++)
-            {
-                if(Q[i] % 11 == 0)
-                {
-                    System.out.println(+Q[i]+ " is divisible by 11.");
-                }
-                else
-                {
-                    System.out.println(+Q[i]+ " is not divisible by 11");
-                }
-            }
-            break;
-
-            default:
-            System.out.println("Sorry user, you have entered an invalid option !!!!!");
         }
         sc.close();
     }
