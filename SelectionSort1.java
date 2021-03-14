822be7805887957c0eed32fd1c2f8d9f0c06f4ee
yeshwanthvr <yeshwanthvr@hotmail.com>
2021-03-14 20:38:24 +0530

    Another sorting program to sort the numbers.

---
+++ SelectionSort1.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class SelectionSort1
+{
+    public static void main(String args[])
+    {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("How many numbers do you want to sort in an array : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter " +n+ " numbers : ");
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0; i < arr.length-1; i++)
+        {
+            int highest = i;
+            for(int j = i + 1; j < arr.length; j++)
+            {
+                if(arr[j] < arr[highest])
+                highest = j;
+            }
+            int temp = arr[i];
+            arr[i] = arr[highest];
+            arr[highest] = temp;
+        }
+        System.out.println("\nThe sorted array (in ascending order) :- ");
+        for(int i = 0; i < n; i++)
+        {
+            System.out.print(+arr[i]+ " ");
+        }
+    sc.close();
+    }
+}
