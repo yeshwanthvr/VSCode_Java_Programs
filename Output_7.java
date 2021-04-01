1ade5933711f56ab723cb14d0d6f2fedc6d2afb6
yeshwanthvr <yeshwanthvr@hotmail.com>
2021-04-01 14:10:08 +0530

    New program based on the sum of a series

---
+++ Output_7.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Output_7
+{
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("This program is used to print the sum of cubes from 1 to n.");
+        System.out.println("\nEnter the value of n : ");
+        int n = sc.nextInt();
+        long sum = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            sum += Math.pow(i,3);
+        }
+        System.out.println("\n\nThe sum of cubes from 1 to " +n+ " = " +sum);
+        sc.close();
+    }
+}
