dc23752c859e08b4472aad0cfa675cb4069f5f55
Yeshwanth VR <vryeshwanth06@gmail.com>
2021-05-23 14:58:55 +0530

    Changed the name of this program

--- Tables_1.java
+++ Tables_1.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Tables {
+public class Tables_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter five numbers : ");
